553a1c505fd6ced23ce7f630f503d7939f0bb1fe
Crowdbotics <team@crowdbotics.com>
2020-10-26 22:09:22 +0000

    Initial commit.

---
+++ backend/autism_speaks_21986/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for autism_speaks_21986 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autism_speaks_21986.settings')
+
+application = get_wsgi_application()
