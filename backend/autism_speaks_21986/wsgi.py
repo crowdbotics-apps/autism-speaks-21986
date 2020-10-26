f05242550ada5459c912fef1b47478445bf68c16
Crowdbotics <team@crowdbotics.com>
2020-10-26 22:10:27 +0000

    Update models

--- backend/autism_speaks_21986/wsgi.py
+++ backend/autism_speaks_21986/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autism_speaks_21986.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autism_speaks_21986.settings")
 
 application = get_wsgi_application()
